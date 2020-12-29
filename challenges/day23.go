862bcbd23cf1307c0fbe69907adcadd12a6a58ae
Dave Hilton <daveh@qualtrics.com>
2020-12-29 16:32:43 -0700

    day23

--- challenges/day23.go
+++ challenges/day23.go
@@ -18,103 +18,146 @@ func day23_part1(input []string) (string, error) {
 
 	log.Debugf("-- final --\nCups: %s\n", cups)
 
-	var oneIdx int
-
-	for i, v := range cups.Cups {
-		if v == 1 {
-			oneIdx = i
-			break
-		}
+	var cup = cups.Current
+	for cup.Label != 1 {
+		cup = cup.Next
 	}
 
 	var b strings.Builder
-	for i := (oneIdx + 1) % len(cups.Cups); i != oneIdx; i = (i + 1) % len(cups.Cups) {
-		b.WriteString(fmt.Sprintf("%d", cups.Cups[i]))
+	cup = cup.Next
+	for cup.Label != 1 {
+		b.WriteString(fmt.Sprintf("%d", cup.Label))
+		cup = cup.Next
 	}
 
 	return fmt.Sprintf("%s", b.String()), nil
 }
 
 func day23_part2(input []string) (string, error) {
-	_ = day23_parse(input)
-	var result int
+	cups := day23_parse(input)
 
-	log.Debugf("Result: %d\n", result)
+	// find the "last" node
+	cup := cups.Current
+	for cup.Next != cups.Current {
+		cup = cup.Next
+	}
+
+	// append the rest of the million cups
+	last := cups.MaxVal + 1000000 - len(cups.CupMap)
+	for i := cups.MaxVal + 1; i <= last; i++ {
+		cup.Next = &day23_cup{
+			Label: i,
+		}
+		cup = cup.Next
+		cups.CupMap[i] = cup
+		cups.MaxVal = i
+	}
+	cup.Next = cups.Current
+
+	var debugWasEnabled = log.DebugEnabled()
+	for i := 1; i <= 10000000; i++ {
+		if (i < 5 || i%1000000 == 0 || i > 9999995) && debugWasEnabled {
+			log.EnableDebugLogs(true)
+			log.Debugf("-- move %8d --\n", i)
+		}
+
+		cups.Move()
+
+		if (i < 5 || i%1000000 == 0 || i > 9999995) && debugWasEnabled {
+			log.EnableDebugLogs(false)
+		}
+	}
+	log.EnableDebugLogs(debugWasEnabled)
+
+	cup = cups.Current
+	for cup.Label != 1 {
+		cup = cup.Next
+	}
+
+	result := cup.Next.Label * cup.Next.Next.Label
+	log.Debugf("Max value: %d\n", cups.MaxVal)
+	log.Debugf("Next two values: %d * %d = %d\n", cup.Next.Label, cup.Next.Next.Label, result)
+
 	return fmt.Sprintf("%d", result), nil
 }
 
+type day23_cup struct {
+	Label int
+	Next  *day23_cup
+}
+
 type day23_cups struct {
-	Cups     []int
-	CurIndex int
-	MinVal   int
-	MaxVal   int
+	Current *day23_cup
+	CupMap  map[int]*day23_cup
+	MinVal  int
+	MaxVal  int
 }
 
 func (c *day23_cups) Move() {
 
 	log.Debugf("Cups: %s\n", c)
-	curVal := c.Cups[c.CurIndex]
-	// tmp := append(make([]int, 0, len(c.Cups)), c.Cups[0:i1+1], c.Cups[c.CurIndex+3:])
-	// pickedUp := c.Cups[c.CurIndex+1 : c.CurIndex+4]
-	pickedUp := make([]int, 0, 3)
-	tmp := make([]int, 0, len(c.Cups))
-	tmp = append(tmp, curVal)
-	for x := (c.CurIndex + 1) % len(c.Cups); x != c.CurIndex; x = (x + 1) % len(c.Cups) {
-		if len(pickedUp) < 3 {
-			pickedUp = append(pickedUp, c.Cups[x])
-		} else {
-			tmp = append(tmp, c.Cups[x])
-		}
-	}
-	log.Debugf("Pick Up: %d, %d, %d\n", pickedUp[0], pickedUp[1], pickedUp[2])
+	curVal := c.Current.Label
 
-	findVal := curVal - 1
-	targIndex := -1
-	for targIndex == -1 {
-		if findVal < c.MinVal {
-			findVal = c.MaxVal
+	// cut out the next 3 nodes
+	pickedUp := c.Current.Next
+	c.Current.Next = pickedUp.Next.Next.Next
+
+	log.Debugf("Pick Up: %d, %d, %d\n", pickedUp.Label, pickedUp.Next.Label, pickedUp.Next.Next.Label)
+
+	// find the insertion target node
+	var targetNode *day23_cup
+	var targetVal = curVal
+
+	for targetNode == nil {
+		targetVal -= 1
+		if targetVal < c.MinVal {
+			targetVal = c.MaxVal
 		}
-		for j := 0; j < len(tmp); j++ {
-			if tmp[j] == findVal {
-				targIndex = j
+
+		if n, ok := c.CupMap[targetVal]; ok {
+			if n != pickedUp && n != pickedUp.Next && n != pickedUp.Next.Next {
+				targetNode = n
 				break
 			}
 		}
-		findVal -= 1
 	}
-	// log.Debugf("Temp: %v\n", tmp)
-	// log.Debugf("Target index: %d\n", targIndex)
-	log.Debugf("Target: %d\n", tmp[targIndex])
+	log.Debugf("Target: %d\n", targetVal)
 
-	c.Cups = append(make([]int, 0, len(c.Cups)), tmp[0:targIndex+1]...)
-	c.Cups = append(c.Cups, pickedUp...)
-	c.Cups = append(c.Cups, tmp[targIndex+1:]...)
+	// insert the "picked up" nodes after the insertion target
+	targetNext := targetNode.Next
+	targetNode.Next = pickedUp
+	pickedUp.Next.Next.Next = targetNext
 
-	for i, v := range c.Cups {
-		if v == curVal {
-			c.CurIndex = (i + 1) % len(c.Cups)
-			break
-		}
-	}
+	// update the "Current" node pointer
+	c.Current = c.Current.Next
 	log.Debugln()
 }
 
 func (c day23_cups) String() string {
 	var b strings.Builder
-	for i, v := range c.Cups {
-		if i == c.CurIndex {
-			b.WriteString(fmt.Sprintf("(%d) ", v))
-		} else {
-			b.WriteString(fmt.Sprintf(" %d  ", v))
-		}
+
+	first := c.Current
+	b.WriteString(fmt.Sprintf("(%d) ", first.Label))
+	cur := first.Next
+	MAX := 10
+	n := 1
+	for cur != first && n < MAX {
+		b.WriteString(fmt.Sprintf(" %d  ", cur.Label))
+		cur = cur.Next
+		n++
+	}
+	if MAX < len(c.CupMap) {
+		b.WriteString(fmt.Sprintf("...  (and %d more)", len(c.CupMap)-MAX))
 	}
 	return strings.TrimSpace(b.String())
 }
 
 func day23_parse(input []string) day23_cups {
-	nums := make([]int, 0, len(input[0]))
 	min := 10
 	max := -1
+	var first *day23_cup
+	var prev *day23_cup
+	var cupMap = make(map[int]*day23_cup)
 	for _, s := range input[0] {
 		n, _ := strconv.Atoi(string(s))
 		if n < min {
@@ -123,12 +166,24 @@ func day23_parse(input []string) day23_cups {
 		if n > max {
 			max = n
 		}
-		nums = append(nums, n)
+
+		c := &day23_cup{
+			Label: n,
+		}
+		if prev == nil {
+			first = c
+		} else {
+			prev.Next = c
+		}
+		prev = c
+		cupMap[n] = c
 	}
+	prev.Next = first
 	return day23_cups{
-		Cups:   nums,
-		MinVal: min,
-		MaxVal: max,
+		Current: first,
+		CupMap:  cupMap,
+		MinVal:  min,
+		MaxVal:  max,
 	}
 }
 
